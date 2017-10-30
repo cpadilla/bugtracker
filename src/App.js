440154676837d4d045b8910d030ce63ef135cf26
cpadilla <christoferpadilla@gmail.com>
2017-10-30 19:45:48 -0400

    fixing references

--- src/App.js
+++ src/App.js
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-// var BugList = React.createClass({
-//     render: function() {
-//         return (
-//             <div>The bug list would come here.</div>
-//         )
-//     }
-// });
-
 class BugList extends React.Component {
     constructor(props) {
         super(props);
