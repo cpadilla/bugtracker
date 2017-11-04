3ef67313bdfcfcc312c73f769bbc4ea3a6ffe8ef
cpadilla <christoferpadilla@gmail.com>
2017-11-04 12:34:35 -0400

    Added props to BugRow and added a few rows with example data

--- src/App.js
+++ src/App.js
@@ -37,9 +37,25 @@ class BugTable extends React.Component {
         super(props);
     }
 
+    renderRow(id, status, priority, owner, title) {
+        return <BugRow id={id} status={status} priority={priority} owner={owner} title={title}/>
+    }
+
     render() {
         return (
-            <div>This will be a table to list all the bugs.</div>
+            <div>
+                <table className="table">
+                    <tr>
+                        <th>ID</th>
+                        <th>STATUS</th>
+                        <th>PRIORITY</th>
+                        <th>OWNER</th>
+                        <th>TITLE</th>
+                    </tr>
+                    {this.renderRow(0, 'New', 'Normal', 'Chris', 'Update css file')}
+                    {this.renderRow(1, 'New', 'Critical', 'Jeff', 'Add submit bug form')}
+                </table>
+            </div>
         );
     }
 }
@@ -52,6 +68,24 @@ class BugAdd extends React.Component {
     render() {
         return (
             <div>This will be a form to add a new bug.</div>
+        );
+    }
+}
+
+class BugRow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.id}</td>
+                <td>{this.props.status}</td>
+                <td>{this.props.priority}</td>
+                <td>{this.props.owner}</td>
+                <td>{this.props.title}</td>
+            </tr>
         );
     }
 }
