163ddcd74386062f19f16b4aea186279d70f93a2
cpadilla <christoferpadilla@gmail.com>
2017-11-04 21:32:22 -0400

    Added new bug submit form
    Added a handler to add a new bug which is passed into the BugAdd component as a property.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@
 class BugList extends React.Component {
     constructor(props) {
         super(props);
-        this.bugData = [
-            {id:0, priority:'New', status:'Normal', owner:'Chris', title:'Update css file'},
-            {id:1, priority:'New', status:'Critical', owner:'Jeff', title:'Add submit bug form'}
-        ];
+        this.state = {
+            bugs: [
+                {id:0, status:'Closed', priority:'Normal', owner:'Chris', title:'Update css file'},
+                {id:1, status:'In Progress', priority:'Critical', owner:'Jeff', title:'Add submit bug form'}
+            ]
+        }
+        this.addBug = this.addBug.bind(this);
+    }
+
+    testNewBug() {
+        var nextId = this.state.bugs.length + 1;
+        this.addBug({id: nextId, priority: 'Normal', status: 'New', owner: 'Chris', title: 'Warning on console'});
+    }
+
+    addBug(bug) {
+        console.log("Adding bug: ", bug);
+        var bugsModified = this.state.bugs.slice();
+        bug.id = this.state.bugs.length + 1;
+        bugsModified.push(bug);
+        this.setState({bugs: bugsModified});
     }
 
     render() {
@@ -14,9 +30,9 @@ class BugList extends React.Component {
                 <h1>Bug Tracker</h1>
                 <BugFilter />
                 <hr />
-                <BugTable bugs={this.bugData}/>
+                <BugTable bugs={this.state.bugs}/>
                 <hr />
-                <BugAdd />
+                <BugAdd onSubmit={this.addBug}/>
             </div>
         );
     }
@@ -37,16 +53,13 @@ class BugFilter extends React.Component {
 class BugTable extends React.Component {
     constructor(props) {
         super(props);
-        this.bugRows = this.props.bugs.map(function(bug) {
-            return <BugRow key={bug.id} id={bug.id} status={bug.status} priority={bug.priority} owner={bug.owner} title={bug.title}/>
-        });
-    }
-
-    renderRow(bug) {
-        return <BugRow key={bug.id} id={bug.id} status={bug.status} priority={bug.priority} owner={bug.owner} title={bug.title}/>
     }
 
     render() {
+        console.log("BugTable render()");
+        this.bugRows = this.props.bugs.map(function(bug) {
+            return <BugRow key={bug.id} id={bug.id} status={bug.status} priority={bug.priority} owner={bug.owner} title={bug.title}/>
+        });
         return (
             <div>
                 <table className="table">
@@ -69,11 +82,57 @@ class BugTable extends React.Component {
 class BugAdd extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('handle submit');
+        var form = document.forms.newBugForm;
+        this.props.onSubmit({id: 0, owner: form.owner.value, title: form.title.value, status: 'New', priority: form.priority.value});
+        // clear the form for th next input
+        form.owner.value = '';
+        form.title.value = '';
+        form.priority.value = 'Normal'
+
     }
 
     render() {
         return (
-            <div>This will be a form to add a new bug.</div>
+            <div>
+                <form name="newBugForm" onSubmit={this.handleSubmit}>
+                <table>
+                    <tr>
+                        <th>Priority</th>
+                        <th>Owner</th>
+                        <th>Title</th>
+                        <th></th>
+                    </tr>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <select name="priority" value="Normal">
+                                    <option value="Low">Low</option>
+                                    <option value="Normal">Normal</option>
+                                    <option value="High">High</option>
+                                    <option value="Critical">Critical</option>
+                                </select>
+                            </td>
+                            <td>
+                                <input type="text" name="owner"/>
+                            </td>
+                            <td>
+                                <input type="text" name="title"/>
+                            </td>
+                            <td>
+                                <input type="submit" value="Add Bug" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </form>
+            </div>
         );
     }
 }
